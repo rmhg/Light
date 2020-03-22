e056ae09d30d2086dfdc341dfd30d880678a8590
rmhg <39510036+rmhg@users.noreply.github.com>
2020-03-22 14:26:03 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,81 @@
+//CREATED JS BY RAHUL MISHRA
+var canvas = document.querySelector("canvas");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+var pen = canvas.getContext("2d");
+var side = 10;
+function rand(low,high)
+{
+    return Math.floor(Math.random()*high + low);
+}
+function distance(x1,x2,y1,y2)
+{
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    return Math.sqrt(dx*dx + dy*dy);
+}
+class light{
+    constructor(x,y)
+    {
+    
+        this.x = x;
+        this.y = y;
+        this.intensity = 100;
+    }
+    netint(x,y)
+    {
+       var t = distance(this.x,x,this.y,y);
+        var neti;
+        neti = t/(this.intensity);
+        console.log(neti);
+      return neti;
+    }
+}
+class Square
+{
+    constructor(x,y,color = "black",alpha = 1)
+    {
+        this.x = x;
+        this.y = y;
+        this.side = side;
+        this.color = color;
+        this.alpha = alpha;
+        this.draw();
+    }
+    draw()
+    {
+        pen.save();
+        pen.beginPath()
+        pen.rect(this.x,this.y,this.side,this.side);
+        pen.fillStyle = this.color;
+        pen.globalAlpha = this.alpha; 
+        pen.fill();
+        pen.closePath();
+        pen.restore();
+    }
+}
+var l = new light();
+function drawall(l)
+{
+    
+    for(var i = 0;i <= canvas.width;i+=side)
+    {
+        for(var j = 0;j <= canvas.height;j+=side)
+        {
+            var c = l.netint(i,j);
+            new Square(i,j,"black",c);
+        }
+    }
+}
+
+function loop(x,y)
+{
+    pen.clearRect(0,0,canvas.height,canvas.width);
+    drawall(new light(x,y));
+}
+window.onmousemove = ()=>{
+    loop(Math.floor(event.clientX),Math.floor(event.clientY));   
+}
+window.addEventListener("touchmove",()=>{
+    loop(event.touches[0].clientX,event.touches[0].clientY);
+});
